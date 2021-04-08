e15d998f2d9452f01e3d0186ad7c1ae36aeb2443
Mgoumane <goumane.mahmoudou@gmail.com>
2021-04-08 10:16:23 +0200

    2 tableaux ok pour l'agenda

---
+++ front_end/hiver_solidaire/src/views/Agenda.jsx
@@ -0,0 +1,232 @@
+import React from 'react'
+
+function Agenda() {
+
+    const firstActivity = "Fournir et partager le petit déjeuner";
+    const secondActivity = "Préparer et partager le repas,";
+    const thirdActivity = "Préparer le repas chez soi et le tenir à disposition";
+    const fourthActivity = "Récupérer le repas et le partager";
+    const fifthActivity = "Passer la nuit";
+
+
+    return (
+        <>
+
+            <table class="table table2">
+                <thead>
+                    <tr>
+                        <th scope="col">Créneaux</th>
+                        <th scope="col">Lundi</th>
+                        <th scope="col">Mardi</th>
+                        <th scope="col">Mercredi</th>
+                        <th scope="col">Jeudi</th>
+                        <th scope="col">Vendredi</th>
+                        <th scope="col">Samedi</th>
+                        <th scope="col">Dimanche</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+
+                        <td>
+                            <div class="input-group">
+                                <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                                    <option selected>Choose...</option>
+                                    <option value="1">firstActivity</option>
+                                    <option value="2">secondActivity</option>
+                                    <option value="3">thirdActivity</option>
+                                    <option value="3">fourthActivity</option>
+                                    <option value="3">fifthActivity</option>
+                                </select>
+                                <button class="btn btn-outline-secondary" type="button">Send</button>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="input-group">
+                                <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                                    <option selected>Choose...</option>
+                                    <option value="1">firstActivity</option>
+                                    <option value="2">secondActivity</option>
+                                    <option value="3">thirdActivity</option>
+                                    <option value="3">fourthActivity</option>
+                                    <option value="3">fifthActivity</option>
+                                </select>
+                                <button class="btn btn-outline-secondary" type="button">Send</button>
+                            </div>
+                        </td>
+                        <td>  <div class="input-group">
+                            <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                                <option selected>Choose...</option>
+                                <option value="1">firstActivity</option>
+                                <option value="2">secondActivity</option>
+                                <option value="3">thirdActivity</option>
+                                <option value="3">fourthActivity</option>
+                                <option value="3">fifthActivity</option>
+                            </select>
+                            <button class="btn btn-outline-secondary" type="button">Send</button>
+                        </div></td>
+                        <td>  <div class="input-group">
+                            <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                                <option selected>Choose...</option>
+                                <option value="1">firstActivity</option>
+                                <option value="2">secondActivity</option>
+                                <option value="3">thirdActivity</option>
+                                <option value="3">fourthActivity</option>
+                                <option value="3">fifthActivity</option>
+                            </select>
+                            <button class="btn btn-outline-secondary" type="button">Send</button>
+                        </div></td>
+                        <td>  <div class="input-group">
+                            <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                                <option selected>Choose...</option>
+                                <option value="1">firstActivity</option>
+                                <option value="2">secondActivity</option>
+                                <option value="3">thirdActivity</option>
+                                <option value="3">fourthActivity</option>
+                                <option value="3">fifthActivity</option>
+                            </select>
+                            <button class="btn btn-outline-secondary" type="button">Send</button>
+                        </div></td>
+                        <td>  <div class="input-group">
+                            <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                                <option selected>Choose...</option>
+                                <option value="1">firstActivity</option>
+                                <option value="2">secondActivity</option>
+                                <option value="3">thirdActivity</option>
+                                <option value="3">fourthActivity</option>
+                                <option value="3">fifthActivity</option>
+                            </select>
+                            <button class="btn btn-outline-secondary" type="button">Send</button>
+                        </div></td>
+                        <td>  <div class="input-group">
+                            <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                                <option selected>Choose...</option>
+                                <option value="1">firstActivity</option>
+                                <option value="2">secondActivity</option>
+                                <option value="3">thirdActivity</option>
+                                <option value="3">fourthActivity</option>
+                                <option value="3">fifthActivity</option>
+                            </select>
+                            <button class="btn btn-outline-secondary" type="button">Send</button>
+                        </div></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+
+            <table class="table table2">
+                <thead>
+                    <tr>
+                        <th scope="col">Créneaux</th>
+                        <th scope="col">Lundi</th>
+                        <th scope="col">Mardi</th>
+                        <th scope="col">Mercredi</th>
+                        <th scope="col">Jeudi</th>
+                        <th scope="col">Vendredi</th>
+                        <th scope="col">Samedi</th>
+                        <th scope="col">Dimanche</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">4</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                    </tr>
+                    <tr>
+                        <th scope="row">5</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
+    )
+}
+
+export default Agenda;
---
+++ front_end/hiver_solidaire/src/views/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Signup(){
+
+
+    return(
+        <><p>YOU ARE IN THE SIGNUP PAGE</p></>
+    )
+}
+
+export default Signup;
