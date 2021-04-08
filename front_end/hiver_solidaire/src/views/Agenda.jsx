fcdcfbfc430291ca4c3f36c093b45f897ac2f7c3
Mgoumane <goumane.mahmoudou@gmail.com>
2021-04-08 13:53:27 +0200

    Agenda list

--- front_end/hiver_solidaire/src/views/Agenda.jsx
+++ front_end/hiver_solidaire/src/views/Agenda.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
-
+import React from 'react';
+import  { Component, useState } from 'react';
+import Select from 'react-select'
 function Agenda() {
 
-    const firstActivity = "Fournir et partager le petit déjeuner";
-    const secondActivity = "Préparer et partager le repas,";
-    const thirdActivity = "Préparer le repas chez soi et le tenir à disposition";
-    const fourthActivity = "Récupérer le repas et le partager";
-    const fifthActivity = "Passer la nuit";
-
+    const activities = [
+        {
+            value: 1,
+            label: 'Fournir et partager le petit déjeuner'
+        },
+        {
+            value: 2,
+            label: 'Préparer et partager le repas'
+        },
+        {
+            value: 3,
+            label: 'Préparer le repas chez soi et le tenir à disposition'
+        },
+        {
+            value: 4,
+            label: 'Récupérer le repas et le partager'
+        },
+        {
+            value: 5,
+            label: 'Passer la nuit'
+        }
+    ];
+    const [result, choixValue] = useState(activities.label);
+    const choixHandle = e => {
+        choixValue(e.label);
+        console.log(result, choixValue)
+    }
 
     return (
         <>
-
             <table class="table table2">
                 <thead>
                     <tr>
@@ -27,32 +48,30 @@ function Agenda() {
                 </thead>
                 <tbody>
                     <tr>
-                        <th scope="row">1</th>
-
+                        <th scope="row" />
                         <td>
-                            <div class="input-group">
-                                <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
-                                    <option selected>Choose...</option>
-                                    <option value="1">firstActivity</option>
-                                    <option value="2">secondActivity</option>
-                                    <option value="3">thirdActivity</option>
-                                    <option value="3">fourthActivity</option>
-                                    <option value="3">fifthActivity</option>
-                                </select>
-                                <button class="btn btn-outline-secondary" type="button">Send</button>
-                            </div>
+                            <Select
+                                options={activities}
+                                onChange={choixHandle} />
+                            <button
+                                className="btn btn-outline-secondary"
+                                type="button"
+                            >Envoyer</button>
                         </td>
                         <td>
                             <div class="input-group">
                                 <select class="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
                                     <option selected>Choose...</option>
-                                    <option value="1">firstActivity</option>
-                                    <option value="2">secondActivity</option>
-                                    <option value="3">thirdActivity</option>
-                                    <option value="3">fourthActivity</option>
-                                    <option value="3">fifthActivity</option>
+                                    <option value="1" >firstActivity</option>
+                                    <option value="2" >secondActivity</option>
+                                    <option value="3" >thirdActivity</option>
+                                    <option value="4" >fourthActivity</option>
+                                    <option value="5" >fifthActivity</option>
                                 </select>
-                                <button class="btn btn-outline-secondary" type="button">Send</button>
+                                <button
+                                    className="btn btn-outline-secondary"
+                                    type="button"
+                                >Envoyer</button>
                             </div>
                         </td>
                         <td>  <div class="input-group">
@@ -112,6 +131,16 @@ function Agenda() {
                         </div></td>
                     </tr>
                     <tr>
+                        <th scope="row">1</th>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
                         <th scope="row">2</th>
                         <td></td>
                         <td></td>
@@ -120,7 +149,6 @@ function Agenda() {
                         <td></td>
                         <td></td>
                         <td></td>
-
                     </tr>
                     <tr>
                         <th scope="row">3</th>
@@ -154,7 +182,6 @@ function Agenda() {
                     </tr>
                 </tbody>
             </table>
-
             <table class="table table2">
                 <thead>
                     <tr>
@@ -170,6 +197,18 @@ function Agenda() {
                 </thead>
                 <tbody>
                     <tr>
+                        <th scope="row"></th>
+                        <td><center>
+                            <p>{result}</p>
+                        </center></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
                         <th scope="row">1</th>
                         <td></td>
                         <td></td>
@@ -178,7 +217,6 @@ function Agenda() {
                         <td></td>
                         <td></td>
                         <td></td>
-
                     </tr>
                     <tr>
                         <th scope="row">2</th>
@@ -189,7 +227,6 @@ function Agenda() {
                         <td></td>
                         <td></td>
                         <td></td>
-
                     </tr>
                     <tr>
                         <th scope="row">3</th>
@@ -200,7 +237,6 @@ function Agenda() {
                         <td></td>
                         <td></td>
                         <td></td>
-
                     </tr>
                     <tr>
                         <th scope="row">4</th>
@@ -211,7 +247,6 @@ function Agenda() {
                         <td></td>
                         <td></td>
                         <td></td>
-
                     </tr>
                     <tr>
                         <th scope="row">5</th>
