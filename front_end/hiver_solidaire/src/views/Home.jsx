1b1bf6551eee26c4407ba7f4c0217dde347dd125
Schenkel <thi.schenkel@gmail.com>
2021-04-12 17:46:23 +0200

    homepage

--- front_end/hiver_solidaire/src/views/Home.jsx
+++ front_end/hiver_solidaire/src/views/Home.jsx
@@ -3,62 +3,36 @@ import "./Home.css";
 
 function Home() {
 
-
     return (
-        //<><p>YOU ARE IN THE HOME PAGE</p></>
-        <div style={{ marginTop: 0 }} className="container-fluid" >
-            <div class="navbar navbar-light bg-light">
-                <div style={{ marginTop: 0 }} className="container-fluid">
-                    <div style={{ fontSize: 12, padding: 0, fontWeight:'bold'}}>
-                        <span >L'EGLISE</span><br></br>
-                        <span>CATHOLIQUE</span><br></br>
-                        <span>A PARIS</span>
-                    </div>
-                    <div >
-                        <ul>
-                            <li style={{ display: "flex", justifyContent: 'space-between' }}>
-                                <a href="#"><i class="fab fa-instagram-square"></i></a>
-                                <a href="#"><i class="fab fa-linkedin"></i></a>
-                                <a href="#"><i class="fas fa-rss"></i></a>
-                            </li>
-                            <li style={{ display: "flex", justifyContent: 'space-between' }}>
-                                <a href="#"><i class="fab fa-facebook"></i></a>
-                                <a href="#"><i class="fab fa-twitter"></i></a>
-                                <a href="#"><i class="fab fa-youtube"></i></a>
-                            </li>
-                        </ul>
 
-                    </div>
-                </div>
+        <div style={{ marginTop: 0 }} className="container" >
 
-            </div>
-           
             <div>
-                <img style={{ width: 1240, heigth: 200 }} src="\imgHS\image-daccueil.jpg"/>
-                <div class="icon-container"> 
-                    <a href="https://www.youtube.com/watch?v=QLO7-CupocE" target="blank"><i style={{ fontSize: 60, textAlignVertical: "center", color:'red' }} class="fab fa-youtube"></i></a>
+                <img style={{ width: 1240, heigth: 200 }} src="\imgHS\image-daccueil.jpg" />
+                <div class="icon-container">
+                    <a href="https://www.youtube.com/watch?v=QLO7-CupocE" target="blank"><i style={{ fontSize: 60, textAlignVertical: "center", color: 'red' }} class="fab fa-youtube"></i></a>
                 </div>
             </div>
 
 
-        < div style = {{ backgroundColor: 'black', width: 1240, heigth: 400 }}>
-    <h2 style={{ color: 'white', height: 95, display: 'flex', alignItems: 'center', paddingLeft: 500 }}>Hiver Solidaire</h2>
+            < div style={{ backgroundColor: 'black', width: 1240, heigth: 400 }}>
+                <h2 style={{ color: 'white', height: 95, display: 'flex', alignItems: 'center', paddingLeft: 500 }}>Hiver Solidaire</h2>
             </div >
 
             <div style={{ paddingLeft: 70, paddingRight: 70 }}>
                 <p style={{ marginTop: 0, fontSize: '1.3em' }} className="text-one-page">
-            "Depuis 12 ans des paroisses se mobilisent chaque hiver
-            à Paris pour accueillir des personnes à la rue, et leur
-            offrir, outre un abri, la possibilité de reprendre confiance
-            en elles-mêmes, grâce aux relations fraternelles établies
-            dans la durée entre elles et avec des paroissiens
-            bénévoles investis dans cette opération"
+                    "Depuis 12 ans des paroisses se mobilisent chaque hiver
+                    à Paris pour accueillir des personnes à la rue, et leur
+                    offrir, outre un abri, la possibilité de reprendre confiance
+                    en elles-mêmes, grâce aux relations fraternelles établies
+                    dans la durée entre elles et avec des paroissiens
+                    bénévoles investis dans cette opération"
             </p>
                 <h2 style={{ textAlign: 'center' }}>Hiver solidaire 2020-2021</h2>
                 <p style={{ marginTop: 0, fontSize: '1.4em' }}>
-            Hiver solidaire ne s’arrête pas avec la pandémie. Les paroisses
-            s’adaptent au protocole sanitaire (attestations, recommandations)
-            pour mettre à l’abri des accueillis en toute sérénité.
+                    Hiver solidaire ne s’arrête pas avec la pandémie. Les paroisses
+                    s’adaptent au protocole sanitaire (attestations, recommandations)
+                    pour mettre à l’abri des accueillis en toute sérénité.
             </p>
                 <h3>Quelques chiffres pour Paris :</h3>
             </div>
@@ -73,11 +47,11 @@ function Home() {
             <div class="container">
                 <div class="row g-2">
                     <div class="col-12">
-                         <div class="p-3 border bg-light">
-                             <h4>avec le soutien de la Fondation Notre Dame.</h4><img style={{width: 400, heigth: 200 }} src="\imgHS\Logo-Fondation-Notre-Dame.png"/></div>
+                        <div class="p-3 border bg-light">
+                            <h4>avec le soutien de la Fondation Notre Dame.</h4><img style={{ width: 400, heigth: 200 }} src="\imgHS\Logo-Fondation-Notre-Dame.png" /></div>
                     </div>
                 </div>
-             </div>
+            </div>
         </div>
 
 
