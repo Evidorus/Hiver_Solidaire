b06a6da66b58b9ebd6bb832322f8909d39af6124
Schenkel <thi.schenkel@gmail.com>
2021-04-12 17:49:52 +0200

    fix conflict

--- back_end/fixtures.js
+++ back_end/fixtures.js
@@ -191,31 +191,4 @@ async function createPlanning() {
     )
 }
 
-<<<<<<< HEAD
 createPlanning();
-=======
-// async function createUser() {
-//     await UserModel.deleteMany({}).exec();
-//     await UserModel.create(
-//         {
-//             nom: "Olicard",
-//             prénom: "Fabien",
-//             age: 24,
-//             email: "fabien.oli@live.fr",
-//             motDePasse: "fafafafa",
-//             role: "B"
-//         },
-//         {
-//             nom: "Jean",
-//             prénom: "Bernard",
-//             age: 56,
-//             email: "bernard.jean@live.fr",
-//             motDePasse: "bebebebe",
-//             role: "A"
-//         }
-//     )
-// }
-
-// createUser();
-createPlanning();
->>>>>>> bah
