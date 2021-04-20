05b9cbb32cd626fc30c92186c0253ae22824e4e4
Damir <damir.sagadbekov@live.fr>
2021-04-20 18:06:21 +0200

    correction

--- front_end/hiver_solidaire/src/App.js
+++ front_end/hiver_solidaire/src/App.js
@@ -2,12 +2,26 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import Navbar from './components/Navbar'
 import 'antd/dist/antd.css'
+import { triggerFocus } from 'antd/lib/input/Input';
 
 function App() {
   const [connected, setConnected] = useState(false)
 
   useEffect(() => {
-    
+    fetch("http://localhost:8000/profil", {
+        headers: {
+          authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+      .then(response => {
+        console.log(response.status);
+        if (response.status === 200) {
+          setConnected(true)
+        } else {
+          setConnected(false)
+        }
+        return response.json()
+      })
 })
   
   return (
